118e24a49b18305db5123fd7f6bedebe904d42c0
HarithKK <134183v@uom.lk>
2018-05-09 13:40:31 +0530

    2,4 panels

--- LCD_Wide/LCD_Wide.ino
+++ LCD_Wide/LCD_Wide.ino
@@ -5,15 +5,15 @@
 #include <SystemFont5x7.h>
 #include <Arial_Black_16.h>
 
-#define W 3
-#define H 3
+#define W 4
+#define H 2
 
 SoftDMD dmd1(W,H);  // DMD controls the entire display
 //oftDMD dmd2(W,H);
 
 #define BRIGHT 150
 
-int width_changer=8;
+int width_changer=18;
 
 
 //SoftwareSerial wifi(2,3);
@@ -25,20 +25,69 @@ void setup() {
   delay(3000);
   Serial.begin(2400);
 
-  message = ReadWifi();// F("{\"Record_time\":\"2018-04-24 12:19:03\",\"Humidity\":\"73\",\"Ext_temp\":\"29\",\"Int_temp\":\"29\",\"Intensity\":\"33\",\"Win_dir\":\"28.71\",\"Win_speed\":\"0\",\"Rain_gauge\":\"0\",\"Pressure\":\"100861\",\"Soil_Moisture\":\"0\"}");// ReadWifi();
   dmd1.setBrightness(BRIGHT);
   dmd1.begin();
+  
+  dmd1.fillScreen(true);
+  delay(100);
+  dmd1.fillScreen(false);
+  delay(100);
+
+  dmd1.selectFont(Arial_Black_16);
+  dmd1.drawString(5,2,"KW Creations ");
+  
+  message = ReadWifi(); //F("{\"Record_time\":\"2018-04-24 12:19:03\",\"Humidity\":\"73\",\"Ext_temp\":\"29\",\"Int_temp\":\"29\",\"Intensity\":\"33\",\"Win_dir\":\"28.71\",\"Win_speed\":\"0\",\"Rain_gauge\":\"0\",\"Pressure\":\"100861\",\"Soil_Moisture\":\"0\"}");// ReadWifi();
+  
 
 
   dmd1.fillScreen(true);
   delay(100);
   dmd1.fillScreen(false);
   delay(100);
+
   
 }
-int count =0;
+int count =1;
 
 void loop() {
+
+  if(count==5){
+    String s =ReadWifiGraph('T');
+    drawGraph("Temp",getValue(10),s);
+    delay(6000);
+    dmd1.fillScreen(false);
+
+    s =ReadWifiGraph('H');
+    drawGraph("Humd",getValue(6),s);
+    delay(6000);
+    dmd1.fillScreen(false);
+
+    s =ReadWifiGraph('G');
+    drawGraph("Rain",getValue(6),s);
+    delay(6000);
+    dmd1.fillScreen(false);
+
+    s =ReadWifiGraph('W');
+    drawGraph("W.Spd",getValue(6),s);
+    delay(6000);
+    dmd1.fillScreen(false);
+
+    s =ReadWifiGraph('I');
+    drawGraph("Inte",getValue(6),s);
+    delay(6000);
+    dmd1.fillScreen(false);
+
+    s =ReadWifiGraph('P');
+    drawGraph("Pres",getValue(6),s);
+    delay(6000);
+    dmd1.fillScreen(false);
+
+    s =ReadWifiGraph('S');
+    drawGraph("So.M",getValue(6),s);
+    delay(6000);
+    dmd1.fillScreen(false);
+  }
+  
   if(count==10){
     message = ReadWifi();
     count=0;
@@ -46,16 +95,33 @@ void loop() {
   drawTemperature(getValue(10));
   drawHumidity(getValue(6));
   
-  drawWDS(getValue(22),getValue(26),getValue(30),17);
+  drawWDS(getValue(22),getValue(26),getValue(30),15);
   
   delay(2000);
+  for(int i=0;i<19;i++){
+    dmd1.scrollY(-1);  
+  }
   dmd1.fillScreen(false);
 
   drawTemperature(getValue(10));
   drawHumidity(getValue(6));
     
-  drawRGP(getValue(34),getValue(38),getValue(18),17);
+  drawRGP(getValue(34),getValue(38),getValue(18),15);
   delay(2000);
+  for(int i=0;i<19;i++){
+    dmd1.scrollY(-1);  
+  }
+  dmd1.fillScreen(false);
+
+  drawTemperature(getValue(10));
+  drawHumidity(getValue(6));
+  
+  drawRNL(getValue(30),getValue(18),15);
+  
+  delay(2000);
+  for(int i=0;i<19;i++){
+    dmd1.scrollY(-1);  
+  }
   dmd1.fillScreen(false);
   count++;
 }
@@ -106,47 +172,71 @@ String ReadWifi(){
   return rec;
 }
 
+String ReadWifiGraph(char c){
+  
+  Serial.println(c);
+  String rec=String("");
+  unsigned long st = millis();
+  uint8_t et =0;
+  while(((millis() - st) < 10000UL) && et == 0) {
+    if(Serial.available()){
+      char c= (char)Serial.read();
+      rec += c;
+      //Serial.write(c);
+      if(c == '+'){
+        et = 1;
+      }
+    }    
+  }
+
+  return rec;
+  
+}
+
+
 
 void drawWDS(String WD,String WS,String RG,int h){
     dmd1.selectFont(SystemFont5x7);
     String s = F("W. Vane : ");
     s.concat(WD);
-    dmd1.drawString(0,0+h,s);
+    dmd1.drawString(10,0+h,s);
 
     s = F("W. Speed : ");
     s.concat(WS);
-    dmd1.drawString(0,9+h,s);
+    dmd1.drawString(10,9+h,s);
 
-    s = F("Rain Fall : ");
-    s.concat(RG);
-    dmd1.drawString(0,18+h,s);
+//    s = F("Rain Fall : ");
+//    s.concat(RG);
+//    dmd1.drawString(0,18+h,s);
 }
 
 void drawRGP(String P,String SM,String L,int h){
     dmd1.selectFont(SystemFont5x7);
     String s = F("Pressure : ");
     s.concat(P);
-    dmd1.drawString(0,h,s);
+    dmd1.drawString(10,h,s);
 
     s = F("Soil M. : ");
     s.concat(SM);
-    dmd1.drawString(0,9+h,s);
+    dmd1.drawString(10,9+h,s);
+
+//    s = F("Solar In : ");
+//    s.concat(L);
+//    dmd1.drawString(0,18+h,s);
+}
+
+void drawRNL(String RG,String L,int h){
+    dmd1.selectFont(SystemFont5x7);
+    
+    String s = F("Rain Fall : ");
+    s.concat(RG);
+    dmd1.drawString(10,h,s);
 
     s = F("Solar In : ");
     s.concat(L);
-    dmd1.drawString(0,18+h,s);
+    dmd1.drawString(10,9+h,s);
 }
 
-//void drawSML(String SM,String L,int h){
-//    String s = F("Soil M. : ");
-//    s.concat(SM);
-//    dmd2.drawString(0,0+h,s);
-//
-//    s = F("Solar In : ");
-//    s.concat(L);
-//    dmd2.drawString(0,9+h,s);
-//}
-//
 void drawTemperature(String temp){
   dmd1.selectFont(Arial_Black_16);
   dmd1.drawString(1+ width_changer,0,temp);
@@ -160,6 +250,43 @@ void drawHumidity(String h){
   h.concat('%');
   dmd1.drawString(50+width_changer,0,h);
   
+}
+
+void drawGraph(String unit,String val,String values){
+
+  String s = values.substring(0,values.length()-1);
+  String p="";
+  int j=0;
+  int vals[10];
+  for(int i=0;i<s.length();i++){
+      if(s[i] == ','){
+          vals[j++] = p.toInt();
+          p="";
+          continue;
+      }
+      p += s[i];
+  }
+  
+  
+  dmd1.selectFont(Arial_Black_16);
+  dmd1.drawString(2,1,unit);
+  dmd1.drawString(2,17,val);
+  dmd1.selectFont(SystemFont5x7);
+  dmd1.drawString(25,20,"(10h)");
+  
+  dmd1.drawLine(70,2,70,28);
+  dmd1.drawLine(70,28,130,28);
+
+  int x1= 70,y1=28;
+
+  for(int i=0;i<10;i++){
+    int y2 = vals[i]/4;
+    y2 = 28-y2;
+    Serial.println(x1);
+    dmd1.drawLine(x1,y1,x1+5,y2);
+    x1 +=5;
+    y1=y2;
+  }
 }
 
 
--- Wifi_ESP/Wifi_ESP.ino
+++ Wifi_ESP/Wifi_ESP.ino
@@ -6,6 +6,7 @@
 char* ssid = "SEC";
 char* password = "sec12Sg920342248";
 char* url ="http://www.slpiot.org/api/getLatestRecordForLCD/5bf82c59-7ec0-4f";
+char* url1="http://slpiot.org/api/getLatestRecordForLCD10/5bf82c59-7ec0-4f/";
 //[{"Record_time":"2018-04-24 12:19:03","Humidity":"73","Ext_temp":"29","Int_temp":"29","Intensity":"33","Win_dir":"28.71","Win_speed":"0","Rain_gauge":"0","Pressure":"100861","Soil_Moisture":"0"}]
 
 ESP8266WiFiMulti WiFiMulti;
@@ -15,7 +16,7 @@ ESP8266WiFiMulti WiFiMulti;
 void setup() {
  Serial.begin(2400);
  WiFi.mode(WIFI_STA);
-
+ Serial.print("AC");
  WiFiMulti.addAP(ssid, password);
 }
 
@@ -25,7 +26,35 @@ void loop() {
     char c = Serial.read();
     if(c=='R'){
       ReadData();
-    }  
+    } 
+
+    if(c=='T'){
+      ReadData("Ext_Temp");
+    }
+
+     if(c=='H'){
+      ReadData("Humidity");
+    } 
+
+    if(c=='I'){
+      ReadData("Intensity");
+    } 
+
+    if(c=='S'){
+      ReadData("Soil_Moisture");
+    } 
+
+    if(c=='P'){
+      ReadData("Pressure");
+    } 
+
+    if(c=='G'){
+      ReadData("Rain_gauge");
+    } 
+
+    if(c=='W'){
+      ReadData("Win_speed");
+    } 
     Serial.flush();
   }
   
@@ -51,6 +80,28 @@ void ReadData(){
     }  
 }
 
+void ReadData(String i){
+
+    String h = String(url1) + i;
+    char u[h.length()];
+    h.toCharArray(u,h.length()+1);
+    // wait for WiFi connection
+    if((WiFiMulti.run() == WL_CONNECTED)) {
+
+        HTTPClient http;
+        http.begin(u); //HTTP
+        int httpCode = http.GET();
+        if(httpCode > 0) {
+            if(httpCode == HTTP_CODE_OK) {
+                String payload = http.getString();
+                USE_SERIAL.println(payload);
+            }
+        } else {
+            USE_SERIAL.println("ERR");
+        }
+        http.end();
+    }  
+}
 String getArray(String h){
   String y="";
   uint8_t ch =0;
